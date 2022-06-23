1f6aa72c480a6372eef3d04fd02f64b7bc04fa25
Bastien MARTIN <bastien.martin@ives.fr>
2022-06-23 19:06:08 +0200

    Moved build vars

--- contrib/ives/scripts/build_asterisk.sh
+++ contrib/ives/scripts/build_asterisk.sh
@@ -1,5 +1,25 @@
 #!/usr/bin/env bash
 
+SCRIPT_DIR=$(readlink -f $(dirname $0))
+ASTERISK_ROOT_DIR=$(cd ${SCRIPT_DIR}/../../../ && pwd)
+
+__ISA_BITS="64"
+_LIBDIR="/usr/lib64"
+__GLOBAL_LDFLAGS="-Wl,-z,relro -Wl,-z,now"
+_DATADIR="/usr/share"
+_LOCALSTATEDIR="/var"
+
+OPT_FLAGS="-DLUA_COMPAT_MODULE -fPIC -Ofast"
+LD_FLAGS="-m${__ISA_BITS} -Wl,--as-needed,--library-path=${_LIBDIR} ${__GLOBAL_LDFLAGS}"
+AST_VAR_RUN_DIR="/run/asterisk"
+MAKE_ARGS="DEBUG= OPTIMIZE= ASTVARRUNDIR=${AST_VAR_RUN_DIR} ASTDATADIR=${_DATADIR}/asterisk ASTVARLIBDIR=${_DATADIR}/asterisk ASTDBDIR=${_LOCALSTATEDIR}/spool/asterisk NOISY_BUILD=1"
+
+export CFLAGS="${OPT_FLAGS}"
+export CXXFLAGS="${OPT_FLAGS}"
+export FFLAGS="${OPT_FLAGS}"
+export LDFLAGS="${LD_FLAGS}"
+export ASTCFLAGS=" "
+
 #
 #   BUILD ASTERISK
 #
